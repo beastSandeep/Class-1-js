dbf9e08cd358875272d6f6b65bb5d02a2b183974
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-09-16 20:58:53 +0530

    functions added

---
+++ functions.js
@@ -0,0 +1,22 @@
+// function chakki(mall) {
+//   if (mall === "gnehu") {
+//     return "Atta";
+//   }
+// }
+
+// console.log(chakki("gnehu"));
+
+function square(num) {
+  return num ** 2;
+}
+console.log(square(4));
+console.log(square(3));
+console.log(square(8));
+
+function powerOf(num, power) {
+  return num ** power;
+}
+
+console.log(powerOf(2, 4));
+console.log(powerOf(8, 5));
+console.log(powerOf(2, 32));
