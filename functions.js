e1566d91de436c341b5e987477533fec99b148c9
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-09-18 07:37:25 +0530

    functions updated

--- functions.js
+++ functions.js
@@ -7,9 +7,11 @@
 // console.log(chakki("gnehu"));
 
 function square(num) {
-  return num ** 2;
+  // num = parameter
+  return num * num;
 }
 console.log(square(4));
+// 4 is argument for num parameter
 console.log(square(3));
 console.log(square(8));
 
@@ -20,3 +22,30 @@ function powerOf(num, power) {
 console.log(powerOf(2, 4));
 console.log(powerOf(8, 5));
 console.log(powerOf(2, 32));
+
+// Types of Functions
+
+// 1. Function Declaration (basic)
+function fucnName(param) {
+  //code
+}
+
+// 2. Function Expression
+const fucnName2 = function (param) {
+  //code
+};
+
+// 3. Arrow Function (most used)
+const fucnName3 = (param) => {
+  //code
+};
+
+function strToArr(str) {
+  const arr = [];
+  for (let i = 0; i < str.length; i++) {
+    arr.push(str[i]);
+  }
+  return arr;
+}
+
+console.log(strToArr("Sandeep"));
