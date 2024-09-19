0850c90225b29f7b122285ad86f64f5e53e13fa9
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-09-19 20:53:44 +0530

    camalize added in functions

--- functions.js
+++ functions.js
@@ -6,46 +6,80 @@
 
 // console.log(chakki("gnehu"));
 
-function square(num) {
-  // num = parameter
-  return num * num;
-}
-console.log(square(4));
-// 4 is argument for num parameter
-console.log(square(3));
-console.log(square(8));
+// function square(num) {
+//   // num = parameter
+//   return num * num;
+// }
+// console.log(square(4));
+// // 4 is argument for num parameter
+// console.log(square(3));
+// console.log(square(8));
 
-function powerOf(num, power) {
-  return num ** power;
-}
+// function powerOf(num, power) {
+//   return num ** power;
+// }
 
-console.log(powerOf(2, 4));
-console.log(powerOf(8, 5));
-console.log(powerOf(2, 32));
+// console.log(powerOf(2, 4));
+// console.log(powerOf(8, 5));
+// console.log(powerOf(2, 32));
 
-// Types of Functions
+// // Types of Functions
 
-// 1. Function Declaration (basic)
-function fucnName(param) {
-  //code
-}
+// // 1. Function Declaration (basic)
+// function fucnName(param) {
+//   //code
+// }
 
-// 2. Function Expression
-const fucnName2 = function (param) {
-  //code
-};
+// // 2. Function Expression (a annonumous function is sored in a variable)
+// const fucnName2 = function (param) {
+//   //code
+// };
 
-// 3. Arrow Function (most used)
-const fucnName3 = (param) => {
-  //code
-};
+// // 3. Arrow Function (most used)
+// const fucnName3 = (param) => {
+//   //code
+// };
 
-function strToArr(str) {
-  const arr = [];
-  for (let i = 0; i < str.length; i++) {
-    arr.push(str[i]);
+// function strToArr(str) {
+//   const arr = [];
+//   for (let i = 0; i < str.length; i++) {
+//     arr.push(str[i]);
+//   }
+//   return arr;
+// }
+
+// console.log(strToArr("Sandeep"));
+
+const camalize = (str) => {
+  // words => [ 'hello', 'my', 'name', 'is', 'anmol' ]
+  const words = str.toLowerCase().split(" ");
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+
+    if (i >= 1) {
+      //  let's say for now word is "name"
+
+      // "name" => "n" => "N"
+      const firstCapitalLetter = word[0].toUpperCase();
+
+      // "name" => "ame"
+      const remaingWord = word.slice(1);
+
+      // "N" + "ame" => "Name"
+      const camelCasedWord = firstCapitalLetter + remaingWord;
+
+      // ["hello", "my" ,"name"] => ["hello", "My" ,"Name"]
+      words[i] = camelCasedWord;
+    }
   }
-  return arr;
-}
 
-console.log(strToArr("Sandeep"));
+  // words => [ 'hello', 'My', 'Name', 'Is', 'Anmol' ]
+  // [ 'hello', 'My', 'Name', 'Is', 'Anmol' ] => helloMyNameIsAnmol
+  const camelCasedString = words.join("");
+
+  return camelCasedString;
+};
+
+const newString = camalize("Hello My name is anmol"); // helloMyNameIsAnmol
+console.log(newString);
