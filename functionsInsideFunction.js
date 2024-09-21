ccdc0266aa4a452cb003661e5289f9c5e6cd6dff
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-09-21 07:11:35 +0530

    functionsInsideFunction modified

---
+++ functionsInsideFunction.js
@@ -0,0 +1,23 @@
+function app() {
+  const myFunc = () => {
+    console.log("hello from myFunc");
+  };
+
+  const add = (num1, num2) => {
+    console.log(num1 + num2);
+  };
+
+  const multiply = (num1, num2) => {
+    console.log(num1 * num2);
+    return num1 * num2;
+  };
+
+  console.log("Inside app");
+
+  const fifteen = multiply(3, 5);
+  console.log(fifteen + 5);
+  myFunc();
+  add(3, 5);
+}
+
+app();
