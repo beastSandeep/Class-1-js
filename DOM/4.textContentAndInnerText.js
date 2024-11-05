fc625c16aa1aca582f3cb12426fc79e879665f9c
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-11-05 07:46:29 +0530

    text content updated

--- DOM/4.textContentAndInnerText.js
+++ DOM/4.textContentAndInnerText.js
@@ -1,16 +1,16 @@
-const h2 = document.getElementById("heading");
-
-// Getting Values
-console.log(h2.innerText); // Mange Your Task
-console.log(h2.textContent); // Mange Your Task Hello
-console.log(h2.innerHTML); // Mange Your Task <span>Hello</span>
-
-// Setting Values
-h2.innerText = "wow";
-
-console.log(h2.innerText); // wow
+const div = document.getElementById("heading");
 
 // textContent: Use for plain text, including child elements.
 // innerText: Use for visible text, considering CSS.
 // innerHTML: Use for HTML content, including tags.
 // outerHTML: Use for the entire HTML structure of the element.
+
+// Getting Values
+console.log(div.innerText); // Mange Your Task
+console.log(div.textContent); // Mange Your Task Hello
+console.log(div.innerHTML); // Mange Your Task <span>Hello</span>
+
+// Setting Values
+div.innerText = "wow";
+
+console.log(div.innerText); // wow
