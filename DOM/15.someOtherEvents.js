c434a078c40ba69ff3a6bf4d0a3c538f61b0afc0
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-11-13 06:52:49 +0530

    some other events added

---
+++ DOM/15.someOtherEvents.js
@@ -0,0 +1,20 @@
+const body = document.getElementsByTagName("body")[0];
+
+// keypress :- occors when a keyboard key pressed
+// body.addEventListener("keypress", (e) => {
+//   body.innerText = e.key;
+// });
+
+// mouseover :- occors when a mouse hoverd
+// document.addEventListener("mouseover", (e) => {
+//   console.log(e);
+// });
+// same thing but diffrent way
+// document.onmouseover = function (e) {
+//   console.log(e);
+// };
+
+// mouseleave :- occors when a mouse leaves
+// document.addEventListener("mouseleave", (e) => {
+//   console.log(e);
+// });
