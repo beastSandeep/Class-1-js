30616027a311bafef097f0f18b8a5776f16cccda
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-12-10 07:40:43 +0530

    escaping box added

---
+++ Projects/escapingBox/script.js
@@ -0,0 +1,30 @@
+const boxs = document.getElementsByClassName("box");
+
+function move(box) {
+  box.addEventListener("mouseenter", () => {
+    const x = Math.round(Math.random() * (window.innerWidth - 50));
+    const y = Math.round(Math.random() * (window.innerHeight - 50));
+
+    box.style.left = `${x}px`;
+    box.style.top = `${y}px`;
+
+    // box.style.backgroundColor = `rgb(${Math.floor(
+    //   Math.random() * 256
+    // )},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+  });
+  // box.addEventListener("click", () => {
+  //   const x = Math.round(Math.random() * (window.innerWidth - 50));
+  //   const y = Math.round(Math.random() * (window.innerHeight - 50));
+
+  //   box.style.left = `${x}px`;
+  //   box.style.top = `${y}px`;
+
+  //   box.style.backgroundColor = `rgb(${Math.floor(
+  //     Math.random() * 256
+  //   )},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+  // });
+}
+
+for (let i = 0; i < boxs.length; i++) {
+  move(boxs[i]);
+}
