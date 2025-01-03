cc936397c85693093a8e220fec8770c217f1f465
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2025-01-03 15:15:39 +0530

    extra added in blackboard

--- Projects/blackBoard/script.js
+++ Projects/blackBoard/script.js
@@ -11,12 +11,51 @@ const pen = document.getElementById("pen");
 const arrow = document.getElementById("arrow");
 const eraser = document.getElementById("eraser");
 
+let activeStroke = 0;
+let activeBackground = 0;
+let activeStrokeWidth = 0;
+let opacity = 100;
+
+const strokes = document.querySelectorAll("#stroke > button");
+strokes.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    strokes.forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+    activeStroke = index;
+  });
+});
+
+const backgrounds = document.querySelectorAll("#background > button");
+backgrounds.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    backgrounds.forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+    activeBackground = index;
+  });
+});
+
+const strokeWidths = document.querySelectorAll("#strokeWidth > button");
+strokeWidths.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    strokeWidths.forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+    activeStrokeWidth = index;
+  });
+});
+
+const opacityInput = document.querySelector("#opacity");
+opacityInput.addEventListener("change", (e) => {
+  opacity = e.target.value;
+});
+
 const tools = [pointer, pen, arrow, eraser];
 
 let activeToolId = 2;
 let dragging = false; // not clicked
-let radius = 5;
-const lineWidth = radius * 2;
+let radius = 2;
+const lineWidth = 5;
+
+ctx.lineCap = ctx.lineJoin = "round";
 
 function colorChanger(id) {
   tools.forEach((tool, index) => {
@@ -26,6 +65,17 @@ function colorChanger(id) {
       tool.style = "";
     }
   });
+}
+
+function drawLine(x, y) {
+  ctx.lineTo(x, y);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.lineTo(x, y);
+  // ctx.arc(x, y, radius, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.beginPath();
+  ctx.moveTo(x, y);
 }
 
 pointer.addEventListener("click", () => {
@@ -52,10 +102,10 @@ eraser.addEventListener("click", () => {
   canvas.style.cursor = "auto";
 });
 
-function init() {
-  colorChanger(activeToolId);
-}
-init();
+canvas.addEventListener("mouseleave", () => {
+  dragging = false;
+  ctx.beginPath();
+});
 
 canvas.addEventListener("mouseup", () => {
   dragging = false;
@@ -65,23 +115,32 @@ canvas.addEventListener("mousedown", () => {
   dragging = true;
 });
 
-ctx.fillStyle = "white";
-ctx.strokeStyle = "white";
-ctx.lineWidth = lineWidth;
 canvas.addEventListener("mousemove", (e) => {
+  ctx.strokeStyle = strokes[activeStroke].dataset.color.replace(
+    "alpha",
+    opacity / 100
+  );
+  ctx.fillStyle = backgrounds[activeBackground].dataset.color.replace(
+    "alpha",
+    opacity / 100
+  );
+  ctx.lineWidth = Math.round(
+    lineWidth * Number(strokeWidths[activeStrokeWidth].dataset.width)
+  );
+
   if (dragging) {
     if (activeToolId === 2) {
-      ctx.lineTo(e.clientX, e.clientY);
-      ctx.stroke();
-      ctx.beginPath();
-      ctx.lineTo(e.clientX, e.clientY);
-      ctx.arc(e.clientX, e.clientY, radius, 0, Math.PI * 2);
-      ctx.fill();
-      ctx.beginPath();
-      ctx.moveTo(e.clientX, e.clientY);
+      drawLine(e.clientX, e.clientY);
     }
   }
 });
 
-// if (activeToolId === 2) {
-// }
+function init() {
+  colorChanger(activeToolId);
+  canvas.style.cursor = "crosshair";
+  ctx.fillStyle = ctx.strokeStyle = strokes[activeStroke].dataset.color;
+  strokes[activeStroke].classList.add("active");
+  backgrounds[activeBackground].classList.add("active");
+  strokeWidths[activeStrokeWidth].classList.add("active");
+}
+init();
