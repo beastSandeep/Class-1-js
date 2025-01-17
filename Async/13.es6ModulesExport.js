7806d6545217a1dc18199874b8fd2690b147fbe8
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2025-01-17 07:59:42 +0530

    es6 moudule added

---
+++ Async/13.es6ModulesExport.js
@@ -0,0 +1,8 @@
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+export const hello = "hello ji";
+export function wow() {
+  console.log("wow");
+}
+
+export default url;
---
+++ Async/13.es6ModulesImport.js
@@ -0,0 +1,4 @@
+import link, { wow as fn, hello as variable } from "./13.es6ModulesExport.js";
+console.log(link);
+fn();
+console.log(variable);
